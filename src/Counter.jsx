9f9c50faa42e2ea08f0e5605cc4b85409f11abcf
Adam Issam <issamadam15@gmail.com>
2019-11-16 13:03:27 +0100

    commit

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,21 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function Counter() {
   // Déclare une nouvelle variable d'état, qu’on va appeler « count »
   // Déclare une fonction setCount qui permet de modifier la valeur de count
-  const [count, setCount] = useState();
- 
+  const [count, setCount] = useState(0);
 
+  let handleChange = event => {
+    setCount(eval(event.target.value));
+  };
   return (
     <div>
-      <input type='number' placeholder='start counter' value={count} onChange={(e) =>setCount(e.target.value) } />
-      <p>Le compteur est  à : {count} </p>
-      <button onClick={() => setCount(count + 1)}>
-        +1
-      </button>
-      <button onClick={() => setCount(count - 1)}>
-        -1
-      </button>
+      <form className="MyTitleForm">
+        <label htmlFor="title">Enter number </label>
+        <input type="number" onChange={handleChange} />
+      </form>
+      <p>Le compteur est à : {count} </p>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(count - 1)}>-1</button>
     </div>
   );
 }
